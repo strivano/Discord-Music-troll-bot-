fa8993a1d649c4d2b283b009da5ab9f5bbef1932
Ivano <94831844+strivano@users.noreply.github.com>
2022-02-21 18:02:21 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Discord-Music-troll-bot-
+# Music Selfbot v2
+
+Discord SelfBot for playing music in voice channels, useful to troll friends!
+Built in py3 using discord.py
+
+# Run the bot
+git clone https://github.com/strivano/music-selfbot
+cd music-selfbot
+pip install requirements.txt
+replace the 'YOUR-TOKEN' string with your actual bot token
+python3 Bot.py
+
+# Commands
+>play, >musiclist, >credits
+You can easily add more music if you know discord.py basics.
+💕 Enjoy!
+
+# Preview
+![2022-01-08_02-24](https://user-images.githubusercontent.com/89864563/148626074-034884ce-ba3e-4490-877b-24dd6710859d.png)
+
+![2022-01-08_02-24](https://www.youtube.com/watch?v=u8qXQ-b_8Lw)
+
+[Old Version]
+In new version there is >play & >musiclist -> Allows you to play diffrent music from 1 command.
+
+# Credits
+-> Main Author: Pera 
+- Created this bot.
+
+-> CoAuthor: Ivano
+- Made this bot a selfbot
+- Added >play, >credits & >musiclist command
---
+++ bot.py
@@ -0,0 +1,181 @@
+import discord
+from discord.ext import commands
+from datetime import datetime
+from colorama import Fore
+from discord.voice_client import VoiceClient
+import time
+
+def get_time():
+	return ('\033[1m' + Fore.LIGHTMAGENTA_EX + datetime.now().strftime("%H:%M:%S") + Fore.WHITE)
+
+prefix = ">"
+acc_token = "token bro"
+bot = commands.Bot(command_prefix=prefix, self_bot=True)
+bot.remove_command("help")
+
+@bot.event
+async def on_ready():
+	print(f"{get_time()}{Fore.LIGHTGREEN_EX} [+] Bot started")
+
+@bot.command(pass_context=True)
+async def musiclist(ctx):
+	await ctx.send("*[+] List: monkey, scare, pokemon, daddy, malamala, golagola, pustinogu, phintro, najlonkesa*")
+
+@bot.command(pass_context=True)
+async def play(ctx, music: str = None):
+	if music == None:
+		await ctx.send(f"** [X] Usage: {prefix}play [MUSIC] | Use {prefix}musiclist to see all available music. |**")
+	elif music == "scare":
+
+		await ctx.send("[+] Sound executing...")
+		print(f"{get_time()}{Fore.WHITE} [+] Sound executing...")
+		channel = ctx.message.author.voice.channel
+		print(f"{get_time()}{Fore.WHITE} [+] Getting the channel...")
+		vc = await channel.connect()
+		print(f"{get_time()} [+] Connected to the channel")
+		print(f"{get_time()} [+] Playing the sound...")
+		vc.play(discord.FFmpegPCMAudio('sound.mp3'))
+		while vc.is_playing():
+			time.sleep(1)
+		await vc.disconnect()
+		print(f"{get_time()} [+] Left the channel")
+		print(f"{get_time()} {Fore.LIGHTGREEN_EX} [+] Sucessfuly pranked")
+	elif music == "monkey":
+		await ctx.send("[+] Sound executing...")
+		print(f"{get_time()}{Fore.WHITE} [+] Sound executing...")
+		channel = ctx.message.author.voice.channel
+		print(f"{get_time()}{Fore.WHITE} [+] Getting the channel...")
+		vc = await channel.connect()
+		print(f"{get_time()} [+] Connected to the channel")
+		print(f"{get_time()} [+] Playing the sound...")
+		vc.play(discord.FFmpegPCMAudio('monkey.mp3'))
+		while vc.is_playing():
+			time.sleep(1)
+		await vc.disconnect()
+		print(f"{get_time()} [+] Left the channel")
+		print(f"{get_time()} {Fore.LIGHTGREEN_EX} [+] Sucessfuly pranked")
+	elif music == "pokemon":
+		await ctx.send("[+] Sound executing...")
+		print(f"{get_time()}{Fore.WHITE} [+] Sound executing...")
+		channel = ctx.message.author.voice.channel
+		print(f"{get_time()}{Fore.WHITE} [+] Getting the channel...")
+		vc = await channel.connect()
+		print(f"{get_time()} [+] Connected to the channel")
+		print(f"{get_time()} [+] Playing the sound...")
+		vc.play(discord.FFmpegPCMAudio('pokemon.mp3'))
+		while vc.is_playing():
+			time.sleep(1)
+		await vc.disconnect()
+		print(f"{get_time()} [+] Left the channel")
+		print(f"{get_time()} {Fore.LIGHTGREEN_EX} [+] Sucessfuly pranked")
+	elif music == "daddy":
+		await ctx.send("[+] Sound executing...")
+		print(f"{get_time()}{Fore.WHITE} [+] Sound executing...")
+		channel = ctx.message.author.voice.channel
+		print(f"{get_time()}{Fore.WHITE} [+] Getting the channel...")
+		vc = await channel.connect()
+		print(f"{get_time()} [+] Connected to the channel")
+		print(f"{get_time()} [+] Playing the sound...")
+		vc.play(discord.FFmpegPCMAudio('daddy.mp3'))
+		while vc.is_playing():
+			time.sleep(1)
+		await vc.disconnect()
+		print(f"{get_time()} [+] Left the channel")
+		print(f"{get_time()} {Fore.LIGHTGREEN_EX} [+] Sucessfuly pranked")
+	elif music == "najlonkesa":
+		await ctx.send("[+] Sound executing...")
+		print(f"{get_time()}{Fore.WHITE} [+] Sound executing...")
+		channel = ctx.message.author.voice.channel
+		print(f"{get_time()}{Fore.WHITE} [+] Getting the channel...")
+		vc = await channel.connect()
+		print(f"{get_time()} [+] Connected to the channel")
+		print(f"{get_time()} [+] Playing the sound...")
+		vc.play(discord.FFmpegPCMAudio('najlonkesa.mp3'))
+		while vc.is_playing():
+			time.sleep(1)
+		await vc.disconnect()
+		print(f"{get_time()} [+] Left the channel")
+		print(f"{get_time()} {Fore.LIGHTGREEN_EX} [+] Sucessfuly pranked")
+	elif music == "golagola":
+		await ctx.send("[+] Sound executing...")
+		print(f"{get_time()}{Fore.WHITE} [+] Sound executing...")
+		channel = ctx.message.author.voice.channel
+		print(f"{get_time()}{Fore.WHITE} [+] Getting the channel...")
+		vc = await channel.connect()
+		print(f"{get_time()} [+] Connected to the channel")
+		print(f"{get_time()} [+] Playing the sound...")
+		vc.play(discord.FFmpegPCMAudio('golagola.mp3'))
+		while vc.is_playing():
+			time.sleep(1)
+		await vc.disconnect()
+		print(f"{get_time()} [+] Left the channel")
+		print(f"{get_time()} {Fore.LIGHTGREEN_EX} [+] Sucessfuly pranked")
+	elif music == "pustinogu":
+		await ctx.send("[+] Sound executing...")
+		print(f"{get_time()}{Fore.WHITE} [+] Sound executing...")
+		channel = ctx.message.author.voice.channel
+		print(f"{get_time()}{Fore.WHITE} [+] Getting the channel...")
+		vc = await channel.connect()
+		print(f"{get_time()} [+] Connected to the channel")
+		print(f"{get_time()} [+] Playing the sound...")
+		vc.play(discord.FFmpegPCMAudio('pustinogu.mp3'))
+		while vc.is_playing():
+			time.sleep(1)
+		await vc.disconnect()
+		print(f"{get_time()} [+] Left the channel")
+		print(f"{get_time()} {Fore.LIGHTGREEN_EX} [+] Sucessfuly pranked")
+	elif music == "malamala":
+		await ctx.send("[+] Sound executing...")
+		print(f"{get_time()}{Fore.WHITE} [+] Jumpscare executing...")
+		channel = ctx.message.author.voice.channel
+		print(f"{get_time()}{Fore.WHITE} [+] Getting the channel...")
+		vc = await channel.connect()
+		print(f"{get_time()} [+] Connected to the channel")
+		print(f"{get_time()} [+] Playing the sound...")
+		vc.play(discord.FFmpegPCMAudio('malamala.mp3'))
+		while vc.is_playing():
+			time.sleep(1)
+		await vc.disconnect()
+		print(f"{get_time()} [+] Left the channel")
+		print(f"{get_time()} {Fore.LIGHTGREEN_EX} [+] Sucessfuly pranked")
+	elif music == "phintro":
+		await ctx.send("[+] Sound executing...")
+		print(f"{get_time()}{Fore.WHITE} [+] Jumpscare executing...")
+		channel = ctx.message.author.voice.channel
+		print(f"{get_time()}{Fore.WHITE} [+] Getting the channel...")
+		vc = await channel.connect()
+		print(f"{get_time()} [+] Connected to the channel")
+		print(f"{get_time()} [+] Playing the sound...")
+		vc.play(discord.FFmpegPCMAudio('phintro.mp3'))
+		while vc.is_playing():
+			time.sleep(1)
+		await vc.disconnect()
+		print(f"{get_time()} [+] Left the channel")
+		print(f"{get_time()} {Fore.LIGHTGREEN_EX} [+] Sucessfuly pranked")
+	else:
+		await ctx.send("** Music not found! **")
+#credits
+@bot.command(pas_context=True)
+async def credits(ctx):
+	await ctx.send("[+] Check your console.")
+	print(f"{get_time()} "+ Fore.RED + f"Music Selfbot Credits\n- Start Code/Bot by Pera\n- Selfbot Ability, All in one cmd [{prefix}play] by Ivano")
+
+
+bot.run(acc_token, bot=False)
+
+
+
+"""
+ Bot Made by Pera || Selfbot + all in one cmd made by ivano!
+Bot Made by Pera || Selfbot + all in one cmd made by ivano!
+ Bot Made by Pera || Selfbot + all in one cmd made by ivano!
+Bot Made by Pera || Selfbot + all in one cmd made by ivano!
+ Bot Made by Pera || Selfbot + all in one cmd made by ivano!
+Bot Made by Pera || Selfbot + all in one cmd made by ivano!
+ Bot Made by Pera || Selfbot + all in one cmd made by ivano!
+Bot Made by Pera || Selfbot + all in one cmd made by ivano!
+ Bot Made by Pera || Selfbot + all in one cmd made by ivano!
+Bot Made by Pera || Selfbot + all in one cmd made by ivano!
+ Bot Made by Pera || Selfbot + all in one cmd made by ivano!
+Bot Made by Pera || Selfbot + all in one cmd made by ivano!
+"""
